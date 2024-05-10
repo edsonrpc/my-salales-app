a82a5ec7d6421dd36eb079fc03fd77beffe28692
c1311150 Edson Rodrigo Pinho Costa <c1311150@interno.bb.com.br>
2024-05-10 16:40:28 -0300

    categorias e dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'categorias',
+        component: CategoriasComponent
+    },
+    {
+        path: '',
+        component: DashboardComponent
+    }
+];
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+
+interface MenuItem {
+  /**
+  * Caminho a ser carregado quando houver o evento de click no menu  
+  */
+  path: string;
+  /**
+  * Texto que será exibido no item de menu 
+  */
+  label: string;
+}
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [MatListModule,],
+  template: `
+    @for (item of menuItens; track item.path) {
+        <a mat-list-item [href]="item.path">{{ item.label }}</a>
+      }
+  `,
+  styles: ``
+})
+export class MenuComponent {
+
+  menuItens: Array<MenuItem> = [
+    {
+      path: '/',
+      label: 'Home'
+    },
+    {
+      path: '/categorias',
+      label: 'Categorias'
+    },
+    {
+      path: '/fornecedores',
+      label: 'Fornecedores'
+    }
+  ]
+
+}
