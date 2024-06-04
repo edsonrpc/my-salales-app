866ae51bff6120883621c2dc3c2005ca79ca9ac4
c1311150 Edson Rodrigo Pinho Costa <c1311150@interno.bb.com.br>
2024-06-04 15:30:28 -0300

    finalizando categorias com deploy

--- src/app/components/categorias/categorias.component.ts
+++ src/app/components/categorias/categorias.component.ts
@@ -1,23 +1,23 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatTableModule, MatTable, MatTableDataSource } from '@angular/material/table';
-import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
-import { MatSortModule, MatSort } from '@angular/material/sort';
-import { CategoriasItem } from './categorias-datasource';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { Categoria } from './categoria.dto';
-import { CategoriasService } from './categorias.service';
-import { lastValueFrom } from 'rxjs';
-import { CategoriaFormComponent } from './form/categoria-form.component';
-import { MatIconModule } from '@angular/material/icon';
-import { LoadingBarComponent } from '../../loading-bar.component';
+import { AfterViewInit, Component, ViewChild } from '@angular/core'
+import { MatTableModule, MatTable, MatTableDataSource } from '@angular/material/table'
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator'
+import { MatSortModule, MatSort } from '@angular/material/sort'
+import { CategoriasItem } from './categorias-datasource'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { Categoria } from './categoria.dto'
+import { CategoriasService } from './categorias.service'
+import { lastValueFrom } from 'rxjs'
+import { CategoriaFormComponent } from './form/categoria-form.component'
+import { MatIconModule } from '@angular/material/icon'
+import { LoadingBarComponent } from '../../loading-bar.component'
 
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
   styles: `
     .full-width-table {
-      width: 100%;
+      width: 100%
     }
     
   `,
@@ -36,12 +36,12 @@ import { LoadingBarComponent } from '../../loading-bar.component';
 
 
 export class CategoriasComponent implements AfterViewInit {
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<CategoriasItem>;
-  dataSource = new MatTableDataSource<Categoria>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator
+  @ViewChild(MatSort) sort!: MatSort
+  @ViewChild(MatTable) table!: MatTable<CategoriasItem>
+  dataSource = new MatTableDataSource<Categoria>()
   categoria!: Categoria
-  showLoading = false;
+  showLoading = false
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
   displayedColumns = ['id', 'name', 'description', 'actions']
@@ -50,17 +50,17 @@ export class CategoriasComponent implements AfterViewInit {
     this.loadCategorias()
   }
 
-  showForm: boolean = false;
+  showForm: boolean = false
   constructor(private categoriasService: CategoriasService) { }
 
   async loadCategorias(): Promise<void> {
-    this.showLoading = true;
+    this.showLoading = true
     const categoria = await lastValueFrom(this.categoriasService.getAll())
     this.dataSource = new MatTableDataSource(categoria)
     this.table.dataSource = this.dataSource
     this.dataSource.sort = this.sort
     this.dataSource.paginator = this.paginator
-    this.showLoading = false;
+    this.showLoading = false
   }
 
   onNovaCategoria() {
@@ -79,18 +79,18 @@ export class CategoriasComponent implements AfterViewInit {
 
   onSave(categoria: Categoria) {
     const saved = lastValueFrom(this.categoriasService.save(categoria))
-    console.log('#### salvei? ', saved);
+    console.log('#### salvei? ', saved)
     this.hideCategoriaForm()
   }
 
   onEditCategorias(catergoria: Categoria) {
     this.categoria = catergoria
-    this.showForm = true;
+    this.showForm = true
   }
 
   async onDeleteCategoria(categoria: Categoria) {
     if (confirm(`Apagar ${categoria.name} com o id ${categoria.id}?`)) {
-      this.showLoading = true;
+      this.showLoading = true
       await lastValueFrom(this.categoriasService.delete(categoria.id))
       this.showLoading = false
       this.loadCategorias()
